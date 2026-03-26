38597b87694f07c0057d5c2bf0d98eb39a5de460
Felipe Viana <felipeviana2501@gmail.com>
2026-03-25 22:02:50 -0300

    Add files via upload

---
+++ exercicio24.py
@@ -0,0 +1,10 @@
+valor=int(input("Digite um valor inteiro: "))
+if valor % 2 == 0:
+        print ("Numero é divisivel por 2")
+else: 
+        print ("Numero não é divisivel por 2")
+
+if valor % 3 == 0:
+        print ("Numero é divisivel por 3")
+else:
+        print ("Numero nao é divisivel por 3")
